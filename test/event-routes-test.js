f28f6a03863b9e79bf7e4d969d14bcd2f2636244
Aaron Krause <aaronjkrause@gmail.com>
2015-03-29 19:21:37 -0700

    create-event test working

--- test/event-routes-test.js
+++ test/event-routes-test.js
@@ -34,7 +34,18 @@ describe('Event route tests', function() {
       done();
     })
   });
-  it('should create an event');
+  it('should create an event', function(done) {
+    chai.request('localhost:3000')
+      .post('/api/events/new_event')
+      .send({name: 'another-test-event', start: 6, end: 8, timelineId: timelineId})
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200)
+        expect(res.body.name).to.eql('another-test-event');
+        expect(res.body._id).to.exist;
+        done();
+      })
+  });
 
   it('should delete an event');
 })
