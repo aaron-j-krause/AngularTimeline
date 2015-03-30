d04d25d7b0035779974d862ee7d8dff97b6dec2d
Aaron Krause <aaronjkrause@gmail.com>
2015-03-29 19:31:52 -0700

    delete event route working

--- test/event-routes-test.js
+++ test/event-routes-test.js
@@ -13,6 +13,7 @@ var mongoose = require('mongoose');
 describe('Event route tests', function() {
   var timelineId;
   var eventId;
+  var testEvent;
 
   before(function(done) {
     chai.request('localhost:3000')
@@ -24,28 +25,39 @@ describe('Event route tests', function() {
           .post('/api/events/new_event')
           .send({name:'test-event', start: 2, end: 5, timelineId: timelineId})
           .end(function(err, res) {
-            eventId = res.body.id;
+            testEvent = res.body;
             done();
-          })
-      })
-  })
+          });
+      });
+  });
+
   after(function(done) {
     mongoose.connection.db.dropDatabase(function() {
       done();
-    })
+    });
   });
+
   it('should create an event', function(done) {
     chai.request('localhost:3000')
       .post('/api/events/new_event')
       .send({name: 'another-test-event', start: 6, end: 8, timelineId: timelineId})
       .end(function(err, res) {
         expect(err).to.eql(null);
-        expect(res).to.have.status(200)
+        expect(res).to.have.status(200);
         expect(res.body.name).to.eql('another-test-event');
         expect(res.body._id).to.exist;
         done();
-      })
+      });
   });
 
-  it('should delete an event');
-})
+  it('should delete an event', function(done) {
+    chai.request('localhost:3000')
+      .delete('/api/events/' + testEvent._id)
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.body.events).to.not.contain(testEvent);
+        done();
+      });
+  });
+});
