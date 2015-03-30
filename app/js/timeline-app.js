bc76f300911600c65ca66b8edb4bc4c003dba587
Aaron Krause <aaronjkrause@gmail.com>
2015-03-29 20:11:20 -0700

    basics for routing in place

--- app/js/timeline-app.js
+++ app/js/timeline-app.js
@@ -9,13 +9,4 @@ require('./timelines/services/timelineService')(app);
 //controllers
 require('./timelines/controllers/timelineController')(app);
 
-timelineApp.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/home', {
-    templateUrl: 'index.html'
-  })
-  .when('/', function() {
-    redirectTo: '/home'
-  })
-}])
-
 
