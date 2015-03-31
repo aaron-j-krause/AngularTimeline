0dc5d11574f731127cfcfbbc3ed797b633f8493c
Aaron Krause <aaronjkrause@gmail.com>
2015-03-30 20:47:12 -0700

    basic client-side routing working. Main view in home template

--- app/js/timeline-app.js
+++ app/js/timeline-app.js
@@ -1,12 +1,21 @@
 require('angular/angular');
 require('angular-route');
 
-var app = angular.module('timelineApp', []);
-
+var timelineApp = angular.module('timelineApp', ['ngRoute']);
 //services
-require('./timelines/services/timelineService')(app);
+require('./timelines/services/timelineService')(timelineApp);
 
 //controllers
-require('./timelines/controllers/timelineController')(app);
+require('./timelines/controllers/timelineController')(timelineApp);
 
-
+timelineApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/home', {
+    templateUrl: 'templates/home.html'
+  })
+  .when('/', {
+    redirectTo: '/home'
+  })
+  .when('/signin', {
+    templateUrl: 'templates/sign-in.html'
+  })
+}]);
